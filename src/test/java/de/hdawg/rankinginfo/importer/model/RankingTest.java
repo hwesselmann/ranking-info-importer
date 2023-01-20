1489a62ceac76c8af586a5548e7628b7aa45a461
Hauke Wesselmann <hauke@h-dawg.de>
2023-01-20 11:37:29 +0100

    add tests for models

--- src/test/java/de/hdawg/rankinginfo/importer/model/RankingTest.java
+++ src/test/java/de/hdawg/rankinginfo/importer/model/RankingTest.java
@@ -3,6 +3,8 @@ package de.hdawg.rankinginfo.importer.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -11,8 +13,8 @@ class RankingTest {
   @DisplayName("verify the generated equal-method is correct")
   @Test
   void verifyEquals() {
-    Ranking sutA = Ranking.builder().dtbId("12345678").firstname("Max").lastname("Mustermann").build();
-    Ranking sutB = Ranking.builder().dtbId("12345678").firstname("Max").lastname("Mustermann").build();
+    Ranking sutA = Ranking.builder().dtbId("12345678").firstname("Max").lastname("Mustermann").position(20).points("22").club("Musterstadt").nationality(Nationality.GER).rankingPeriod(LocalDate.of(2022, 1,1)).federation(Federation.BB).yearOfBirthRanking(false).endOfYearRanking(false).overallYouthRanking(false).ageGroup("U12").build();
+    Ranking sutB = Ranking.builder().dtbId("12345678").firstname("Max").lastname("Mustermann").position(20).points("22").club("Musterstadt").nationality(Nationality.GER).rankingPeriod(LocalDate.of(2022, 1,1)).federation(Federation.BB).yearOfBirthRanking(false).endOfYearRanking(false).overallYouthRanking(false).ageGroup("U12").build();
 
     boolean result = sutA.equals(sutB);
     assertTrue(result);
